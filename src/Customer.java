b57e7f09822b1754b6a9b20189bd2ec20b3335a3
1580400 <15840582276@163.com>
2026-05-17 14:54:49 +0800

    test update

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,16 @@
 public class Customer {
+    private String name;
+    private int age;
+    void setName(String name){
+        this.name=name;
+    }
+    String getName(){
+        return name;
+    }
+    void setAge(int age){
+        this.age=age;
+    }
+    int getAge(){
+        return this.age;
+    }
 }
