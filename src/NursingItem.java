19438eada286dbd2a75a8d7e4db84e596857ceb5
SuperCola-p <2075935207@qq.com>
2026-05-17 14:04:41 +0800

    完善NursingItem（getter,setter)

--- src/NursingItem.java
+++ src/NursingItem.java
@@ -20,5 +20,91 @@ public class NursingItem {
     // 逻辑删除标记
     private Boolean isDeleted = false;
 
-    // 构造方法、Getter和Setter省略...
+    public Boolean getDeleted() {
+        return isDeleted;
+    }
+
+    @Override
+    public String toString() {
+        return "NursingItem{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", status='" + status + '\'' +
+                ", execPeriod='" + execPeriod + '\'' +
+                ", execTimes=" + execTimes +
+                ", description='" + description + '\'' +
+                ", isDeleted=" + isDeleted +
+                '}';
+    }
+
+    public void setDeleted(Boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getExecTimes() {
+        return execTimes;
+    }
+
+    public void setExecTimes(Integer execTimes) {
+        this.execTimes = execTimes;
+    }
+
+    public String getExecPeriod() {
+        return execPeriod;
+    }
+
+    public void setExecPeriod(String execPeriod) {
+        this.execPeriod = execPeriod;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    // 构造方法
 }
