41f14eb8b63af6e3bc696aad22ee4bb0490f60e9
SuperCola-p <2075935207@qq.com>
2026-05-31 09:11:17 +0800

    final version

--- src/java/data/adminData.java
+++
@@ -1,8 +0,0 @@
-package java.data;
-import java.entity.admin;
-
-public class adminData {
-    admin admin1= new admin("admin","admin","NEUSOFTadmin1");
-    admin admin2= new admin("admin1","admin1","NEUSOFTadmin2");
-    admin admin3= new admin("admin2","admin2","NEUSOFTadmin3");
-}
